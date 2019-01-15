502fab40da34487fc2b13b6ee222e5b24fe133b3
kkonrad <kkonrad@mail.com>
2019-01-16 00:45:18 +0100

    Removed false throwing declaration

--- src/main/java/kkonrad/simple/web/crawler/Application.java
+++ src/main/java/kkonrad/simple/web/crawler/Application.java
@@ -2,25 +2,23 @@ package kkonrad.simple.web.crawler;
 
 import kkonrad.simple.web.crawler.core.Crawler;
 import kkonrad.simple.web.crawler.core.Link;
-import kkonrad.simple.web.crawler.core.webprocessing.JSoupBasedPageDownloader;
 import kkonrad.simple.web.crawler.core.orchestration.SimpleCrawlingOrchestrator;
 import kkonrad.simple.web.crawler.core.resultsprocessing.LinksCollectingCrawlingResultsProcessor;
+import kkonrad.simple.web.crawler.core.webprocessing.JSoupBasedPageDownloader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.IOException;
 
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
 
         String pageUrl = args[0];
         crawlPage(pageUrl);
     }
 
-    public static void crawlPage(String pageUrl) throws IOException {
+    public static void crawlPage(String pageUrl) {
         Link seedLink = new Link(pageUrl);
 
         Crawler crawler = new Crawler(
